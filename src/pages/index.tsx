801e7fe0a87701c92014bcb1343be8cc58f7f3f7
Rasika Bhalerao <rasikabh@users.noreply.github.com>
2025-07-16 11:47:43 -0700

    Remove TDD

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Hello() {
                 <Heading size='xl'>CS 2100: Program Design and Implementation I</Heading>
                 <Blockquote.Root>
                     <Blockquote.Content>
-                    Building on intro programming experience (from CS 2000 or a different institution), examines the fundamentals of program design and implementation. Studies design of data and object-oriented programs, including common patterns, use of data structures, and underlying principles such as abstraction, encapsulation, inheritance, and interfaces. Introduces common software engineering practices such as test-driven development, version control, development environments, and good programming habits. Practices using these design principles by writing medium-sized applications and using data science code libraries. Continues to interweave ethical challenges and skills in program design.
+                    Building on intro programming experience (from CS 2000 or a different institution), examines the fundamentals of program design and implementation. Studies design of data and object-oriented programs, including common patterns, use of data structures, and underlying principles such as abstraction, encapsulation, inheritance, and interfaces. Introduces common software engineering practices such as version control, writing tests, development environments, and good programming habits. Practices using these design principles by writing medium-sized applications and using data science code libraries. Continues to interweave ethical challenges and skills in program design.
                     </Blockquote.Content>
                 </Blockquote.Root>
                 <Alert.Root status='warning'>
